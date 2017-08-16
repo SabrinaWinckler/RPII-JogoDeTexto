c18516f7407c5ebf44040cedfa98a742051ce69b
Gustavo Satheler <gustavosatheler@gmail.com>
2017-08-15 21:03:44 -0300

    v0.1.1
    Reorganizando pastas

--- TextGame/TextGame/App.xaml.cs
+++ TextGame/TextGame/App.xaml.cs
@@ -17,20 +17,35 @@ using Windows.UI.Xaml.Navigation;
 
 namespace TextGame
 {
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
     sealed partial class App : Application
     {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
         public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
 
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
             if (rootFrame == null)
             {
+                // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
@@ -40,6 +55,7 @@ namespace TextGame
                     //TODO: Load state from previously suspended application
                 }
 
+                // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
@@ -47,8 +63,12 @@ namespace TextGame
             {
                 if (rootFrame.Content == null)
                 {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
+                // Ensure the current window is active
                 Window.Current.Activate();
             }
         }
