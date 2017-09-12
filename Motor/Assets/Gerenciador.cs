9cc8cb002531871075bd5494051a891ff8ff407f
Sabrina <sabrinacarlew@gmail.com>
2017-09-12 15:15:44 -0300

    Motor

---
+++ Motor/Assets/Gerenciador.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.JsonUtility.FromJson;
+using UnityEngine;
+namespace Escolha
+{
+    [System.Serializable]
+    class Gerenciador  
+    {
+        public string texto;
+        public int id;
+        public string ramificacao;
+        public bool resposta;
+        public string consequencia;
+
+        public Gerenciador(int id, string ramificacao, string cena, string texto)
+        {
+            this.id = id;
+            this.ramificacao = ramificacao;
+            this.consequencia = cena;
+            this.texto = texto;
+                      
+        }
+        public Gerenciador()
+        {
+        }
+        public Gerenciador(bool resposta)
+        {
+            this.resposta = resposta;
+        }
+
+
+        public static Gerenciador CreateFromJSON(string json)
+        {
+            return JsonUtility.FromJson<Gerenciador>(json);
+        }
+
+        
+    }
+
+}
---
+++ Motor/Assets/Motor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Escolha;
+
+public class MainPlayer : MonoBehaviour
+{
+    [Serializable]
+    public class Motor
+    {
+
+        void Start()
+        {
+            Gerenciador game = new Gerenciador();
+            string text = System.IO.File.ReadAllText(@"C:\Users\SABRINA\Documents\trabalho1\Motor\Assets\caminhos.json");
+            Gerenciador.CreateFromJSON(text);
+            //do
+            //{
+                System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            //a partit do id texto é mostrado e depois do click é levado a uma nova cena. 
+           // } while (!game.resposta);
+        }
+
+        void Update()
+        {
+            game.resposta = false;
+            // e avança para o próximo objeto instanciado atraves do json 
+        }
+    }
+}
