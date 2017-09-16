61d25f4bc2010420742feac4d5581219b8365176
Michael Martins <martins.m09@hotmail.com>
2017-09-16 01:09:24 -0300

    UML v.1.5 + Console v.1.7

--- Console/TextGame/Item.cs
+++ Console/TextGame/Item.cs
@@ -11,13 +11,10 @@ namespace TextGameTESTE
     {
         //ATRIBUTOS
         private String nome;
-        private String[] item = { "FACA", "GRANADA", "ARMA", "REMEDIO", "LUVA", "BUSSOLA", "IDENTIDADES FALSAS", "PASSAPORTES FALSOS", "OCULOS", "CANETA" };
-        private Random r = new Random();
 
         //COMPORTAMENTOS E METODOS
         public Item()
         {
-            nome = item[r.Next(10)];
         }
 
         public string Nome { get => nome; set => nome = value; }
