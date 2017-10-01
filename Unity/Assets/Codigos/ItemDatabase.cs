237f140410aa0ec5b7a356e289eb94c7a7f58dcf
Rodrigo Oliveira <roliveira.loki@gmail.com>
2017-10-01 06:57:35 -0300

    UI 1.2 - Inventario

--- Unity/Assets/Codigos/ItemDatabase.cs
+++ Unity/Assets/Codigos/ItemDatabase.cs
@@ -6,31 +6,42 @@ using System.IO;
 
 public class ItemDatabase : MonoBehaviour {
 
-    private List<ItemJson> database = new List<ItemJson>();
-    private JsonData itemData;
+    public List<ItemJson> database = new List<ItemJson>();
+    public JsonData itemData;
 
     private void Start()
     {
-        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/ItemJson.json"));
         ConstrucItemDatabase();
 
     }
 
+    public ItemJson FetchItemByID(int id)
+    {
+        for (int i = 0; i < database.Count; i++)
+            if (database[i].ID == id)
+            {
+                return database[i];
+            }
+
+        return null;
+    }
     void ConstrucItemDatabase()
     {
         for (int i = 0; i < itemData.Count; i++)
         {
-            database.Add(new ItemJson((int)itemData[i]["id"], itemData[i]["nome"].ToString(), itemData[i]["Decricao"].ToString(), (int)itemData[i]["valor"]));
+            database.Add(new ItemJson((int)itemData[i]["id"], itemData[i]["nome"].ToString(), itemData[i]["descricao"].ToString(), (int)itemData[i]["valor"]));
         }
     }
 }
 
-public class ItemJson
-{
+ public class ItemJson {
     public int ID { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
     public int Valor { get; set; }
+
+    
 
     public ItemJson(int id, string nome, string descricao, int valor)
     {
