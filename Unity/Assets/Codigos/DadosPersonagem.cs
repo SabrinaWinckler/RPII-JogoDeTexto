7824b4aba8107b20a118e86fe606b7d98e49e554
Sabrina <sabrinacarlew@gmail.com>
2017-10-01 04:56:22 -0300

    Métodos botões

--- Unity/Assets/Codigos/DadosPersonagem.cs
+++ Unity/Assets/Codigos/DadosPersonagem.cs
@@ -20,15 +20,16 @@ namespace textgame
         public Text text_profissao;
         public bool click;
         public bool interacao;
+        Button[] btn;
+        int escolhido;
         //public String profissao;
         #endregion
         void Start()
         {
-            
             personagem = new Personagem();
+            Button[]  btn = {medico, advogado, policial, atleta };
             click = false;
             interacao = false;
-       
             
         }
 
@@ -37,24 +38,22 @@ namespace textgame
         {
             if (!interacao)
             {
-                escolhida();
-
+                ButtonQualquer_Click(medico);
                 this.interacao = true;
             }
 
+
         }
+                
         public void definirProfissao()
         {
-            for (int i = 0; i < personagem.profissoes.Length; i++)
-            {
-
-            }
+            
             
                 if (medico.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
                 {
                     personagem.nome_profissao = medico.name;
                     text_profissao.text = personagem.nome_profissao;
-                    
+                                  
                    
                 }
 
@@ -82,37 +81,56 @@ namespace textgame
                 }
          
         }
-        public void escolhida()
+        public void escolhermedico()
 
         {
-            
-           
-            if (medico.IsActive())
-            {
-                click = true;
+            if (!click) {
+                medico.onClick = new Button.ButtonClickedEvent();
+                medico.onClick.AddListener(() => definirProfissao());
                 personagem.posicao_vetor = 0;
-                medico.onClick.AddListener(definirProfissao);
+                click = true;
             }
-            if (atleta.IsActive())
+            
+            
+            
+        }
+        public void ButtonQualquer_Click(object sender)
+        {
+            personagem.posicao_vetor = 0;
+            Button btnClicado = (Button)sender;
+            text_profissao.text = btnClicado.name;
+        }
+        public void selecionado() {
+
+          
+        }
+        public void escolheratleta() {
+            if (!click)
             {
                 click = true;
                 personagem.posicao_vetor = 1;
-                atleta.onClick.AddListener(definirProfissao);
+                atleta.onClick = new Button.ButtonClickedEvent();
+                atleta.onClick.AddListener(() => definirProfissao());
             }
-            if (policial.IsActive())
+        }
+        public void escolherpolicial() {
+            if (!click)
             {
                 click = true;
-                personagem.posicao_vetor = 0;
-                policial.onClick.AddListener(definirProfissao);
+                personagem.posicao_vetor = 2;
+                policial.onClick = new Button.ButtonClickedEvent();
+                policial.onClick.AddListener(() => definirProfissao());
             }
-            if (advogado.IsActive())
+        }
+        public void escolherAdvogado() {
+            if (!click)
             {
                 click = true;
                 personagem.posicao_vetor = 3;
-                advogado.onClick.AddListener(definirProfissao);
+                advogado.onClick = new Button.ButtonClickedEvent();
+                advogado.onClick.AddListener(() => definirProfissao());
             }
         }
     }
-
 }
 
