ebbf530254ca33ac324da7b69a3e8813ab310526
Sabrina <sabrinacarlew@gmail.com>
2017-09-30 22:16:31 -0300

    Interface Dados personagem

--- Unity/Assets/Codigos/DadosPersonagem.cs
+++ Unity/Assets/Codigos/DadosPersonagem.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace textgame
 {
-   class DadosPersonagem : MonoBehaviour
+    class DadosPersonagem : MonoBehaviour
     {
         #region Atributos 
         [Header("Botões")]
@@ -19,76 +19,100 @@ namespace textgame
         public Personagem personagem;
         public Text text_profissao;
         public bool click;
+        public bool interacao;
         //public String profissao;
         #endregion
         void Start()
         {
-            medico.onClick = new Button.ButtonClickedEvent();
-            atleta.onClick = new Button.ButtonClickedEvent();
-            advogado.onClick = new Button.ButtonClickedEvent();
-            policial.onClick = new Button.ButtonClickedEvent();
+            
             personagem = new Personagem();
-                                          
+            click = false;
+            interacao = false;
+       
+            
         }
 
         // Update is called once per frame
         void Update()
         {
-            medico.onClick.AddListener(definirProfissao);
-            atleta.onClick.AddListener(definirProfissao);
-            advogado.onClick.AddListener(definirProfissao);
-            policial.onClick.AddListener(definirProfissao);
+            if (!interacao)
+            {
+                escolhida();
+
+                this.interacao = true;
+            }
+
         }
-        public void definirProfissao() {
-            for (int i = 0; i < personagem.profissoes.Length; i++) {
-                if (personagem.profissoes[i].Nome.Equals("medico", StringComparison.OrdinalIgnoreCase)) {
-                    personagem.posicao_vetor = i;
-                    break;
-                }
-                if (personagem.profissoes[i].Nome.Equals("atleta", StringComparison.OrdinalIgnoreCase))
-                {
-                    personagem.posicao_vetor = i;
-                    break;
-                }
-                if (personagem.profissoes[i].Nome.Equals("policial", StringComparison.OrdinalIgnoreCase))
-                {
-                    personagem.posicao_vetor = i;
-                    break;
-                }
-                if (personagem.profissoes[i].Nome.Equals("advogado", StringComparison.OrdinalIgnoreCase))
-                {
-                    personagem.posicao_vetor = i;
-                    break;
-                }
+        public void definirProfissao()
+        {
+            for (int i = 0; i < personagem.profissoes.Length; i++)
+            {
 
             }
+            
                 if (medico.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
                 {
                     personagem.nome_profissao = medico.name;
                     text_profissao.text = personagem.nome_profissao;
                     
+                   
                 }
-              
-                if (atleta.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
+
+                else if (atleta.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
                 {
                     personagem.nome_profissao = atleta.name;
                     text_profissao.text = personagem.nome_profissao;
-                    
+                   
+
                 }
-                if (policial.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
+                else if (policial.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
                 {
                     personagem.nome_profissao = policial.name;
                     String aux = personagem.GetProfissao().Nome;
                     text_profissao.text = aux;
                     
+
                 }
-                if (advogado.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
+                else if (advogado.name.Equals(personagem.profissoes[personagem.posicao_vetor].Nome, StringComparison.OrdinalIgnoreCase))
                 {
                     personagem.nome_profissao = advogado.name;
                     String aux = personagem.GetProfissao().Nome;
                     text_profissao.text = aux;
+                    
                 }
+         
+        }
+        public void escolhida()
+
+        {
+            
+           
+            if (medico.IsActive())
+            {
+                click = true;
+                personagem.posicao_vetor = 0;
+                medico.onClick.AddListener(definirProfissao);
+            }
+            if (atleta.IsActive())
+            {
+                click = true;
+                personagem.posicao_vetor = 1;
+                atleta.onClick.AddListener(definirProfissao);
+            }
+            if (policial.IsActive())
+            {
+                click = true;
+                personagem.posicao_vetor = 0;
+                policial.onClick.AddListener(definirProfissao);
+            }
+            if (advogado.IsActive())
+            {
+                click = true;
+                personagem.posicao_vetor = 3;
+                advogado.onClick.AddListener(definirProfissao);
             }
         }
     }
+
+}
 
