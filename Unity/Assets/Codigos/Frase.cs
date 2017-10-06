2ab0630cfc52b42bc2824e2b7b3d3ebce5aa0eeb
Gustavo Satheler <gustavosatheler@gmail.com>
2017-10-06 02:01:11 -0300

    Exibir dicas
    O reconhecimento de palavras exibe dicas a parti de um texto escrito.

---
+++ Unity/Assets/Codigos/Frase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace textgame {
+    public class Frase {
+
+        private string palavra;
+        private string frase;
+
+        public Frase(string palavra, string frase) {
+            this.palavra = palavra;
+            this.frase = frase;
+        }
+
+        public string getPalavra() { 
+            return this.palavra;
+        }
+
+        public string getFrase() {
+            return this.frase;
+        }
+    }
+}
---
+++ Unity/Assets/Codigos/ReconhecimentoPalavra.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace textgame {
+    public class ReconhecimentoPalavra : MonoBehaviour {
+
+        private ArrayList frasesTestes;
+        private ArrayList palavras;
+        private ArrayList palavrasResposta;
+
+        private ArrayList frases;
+
+        public InputField texto_entrada;
+        public Text texto_dica;
+
+        string teste;
+
+        // Use this for initialization
+        void Start() {
+
+            frasesTestes = new ArrayList();
+            palavras = new ArrayList();
+            palavrasResposta = new ArrayList();
+            frases = new ArrayList();
+
+            frasesTestes.Add("Abrir a porta");
+            frasesTestes.Add("Mandar para a puta que pariu");
+            frasesTestes.Add("O JUDSON É GAY");
+        }
+
+        // Update is called once per frame
+        void Update() {
+            this.separarFrase(frasesTestes);
+            this.separarResposta();
+            this.mostrarDica();
+        }
+
+        private void separarFrase(ArrayList frase) {
+            string sentenca;
+            this.palavras.Clear();
+            for (int i = 0; i < frase.Count; i++) {
+                sentenca = (string) frase[i];
+                this.separarPalavra(sentenca.ToUpper());
+            }
+        }
+
+        private void separarPalavra(string frase) {
+            string[] sentenca = frase.Split(' ');
+            for (int i = 0; i < sentenca.Length; i++) {
+                frases.Add(new Frase(sentenca[i], frase));
+                palavras.Add(sentenca[i]);
+            }
+        }
+
+        private void separarResposta() {
+            string[] sentenca = texto_entrada.text.Split(' ');
+            for (int i = 0; i < sentenca.Length; i++) {
+                palavrasResposta.Add(sentenca[i]);
+            }
+        }
+
+        private void mostrarFrases() {
+            string termo;
+            for (int i = 0; i < frases.Count; i++) {
+                termo = ((Frase) frases[i]).getPalavra();
+                if ((texto_entrada.text).ToUpper().Equals(termo)) {
+                    texto_dica.text = ((Frase)frases[i]).getFrase();
+                    Debug.Log("Entrada reconhecida.");
+                }
+            }
+        }
+
+        private void mostrarDica() {
+            if (texto_entrada.text.Length > 2) {
+                this.mostrarFrases();
+            } else {
+                texto_dica.text = "";
+            }
+        }
+    }
+}
